7ff78cc15d9614268211c3f0737c99dabc1ece98
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-04-04 19:49:02 -0700

    establishing the models

--- models/index.js
+++ models/index.js
@@ -1,8 +1,39 @@
 const User = require('./User');
+const Article = require('./Article');
+const Tips = require('./Tips');
+
+User.hasMany(User, {
+    foreignKey: "author_id"
+});
+
+User.hasMany(Tips, {
+   foreignKey: "author_id",
+   onDelete: "CASCADE"
+ 
+});
+
+Article.belongsTo(User, {
+    foriegnKey: "author_id"
+
+});
+
+Tips.belongsTo(User, {
+    foreignKey: "author_id"
+
+});
 
 
 
 
-module.exports = { User };
+
+
+
+
+
+module.exports = { 
+    User, 
+    Article, 
+    Tips,
+ };
 
 // user and articles destructuring (refer index.js in mini28)
