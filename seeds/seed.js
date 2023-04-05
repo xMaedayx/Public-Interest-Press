cf5ffa8fb5ce944cbb72492580661b6c8b2f4e2d
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-04-05 07:34:26 -0700

    adding json files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,34 @@
 const sequelize = require('../config/connection');
+const { User, Article, Tips } = require('../models');
+const userData = require ("../userData.json");
+const tipsData = require("../tipsData.json");
+const articleData = require(".../articleData.json");
+
+
 
 const seedDatabase = async () =>  {
+    await sequelize.sync({ force: true});
 
-}
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
 
-seedDatabase()
+    await Article.bulkCreate(articleData, {
+        individualHooks: true,
+        returning: true,
+
+
+    } );
+
+    await Tips.bulkCreate(tipsData, {
+        individualHooks: true, 
+        returning: true, 
+
+
+    });
+
+process.exit(0);
+};
+
+seedDatabase();
