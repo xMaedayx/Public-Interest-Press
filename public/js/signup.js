169f11c0c2bcf0ba8930fa458baf0c9f0faf311f
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-04-11 18:23:03 -0700

    added login functionality

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,29 +1,25 @@
-const signupFormHandler = async (event) => {
-    event.preventDefault();
+// const signupFormHandler = async (event) => {
+//     event.preventDefault();
 
-    // Collect values from the sign-up form
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+//     const name = document.querySelector('#name-signup').value.trim();
+//     const email = document.querySelector('#email-signup').value.trim();
+//     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && email && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+//     if (name && email && password) {
+//       const response = await fetch('/api/users/signup', {
+//         method: 'POST',
+//         body: JSON.stringify({ name, email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
 
-      if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   };
 
-  // Add event listener to the sign-up form
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+//   document
+//     .querySelector('.signup-form')
+//     .addEventListener('submit', signupFormHandler);
