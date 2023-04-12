90294b74bba7ff99e93442417c39be05030eb863
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-04-12 15:04:07 -0700

    fucntion for button and modal

--- public/js/postBtn.js
+++ public/js/postBtn.js
@@ -1,23 +1,15 @@
-const modal = document.getElementById("myModal1");
-const btn = document.getElementById("pstBtn");
-const span = document.getElementsByClassName("clsos"[0]);
+const button = document .querySelector('.btn');
+const modal = document.querySelector('.modal');
 
-// When the user clicks the button, open the modal
-btn.onclick = function(){
+button.onclick = () => {
+    // log that the button has been clicked 
+    console.log("button clicked");
+    // display the modal 
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function(){
-    modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside the modal, close it
-window.onclick = function(event){
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
 
 $('#upload-button').on('click', function() {
     var fileInput = $('#file-input')[0];
