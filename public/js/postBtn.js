50e046136aa0cc34c47d3f5aaf12d56b1f3ab157
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-04-12 22:52:08 -0700

    stash

--- public/js/postBtn.js
+++ public/js/postBtn.js
@@ -1,3 +1,5 @@
+
+
 $('#upload-button').on('click', function() {
     var fileInput = $('#file-input')[0];
     var file = fileInput.files[0];
@@ -17,3 +19,18 @@ $('#upload-button').on('click', function() {
       }
     });
   });
+
+  $.ajax({
+    url: '/upload', // URL of the server-side endpoint
+    type: 'POST',   // HTTP request method
+    data: formData, // Form data to be sent in the request
+    processData: false,
+    contentType: false,
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus, errorThrown);
+    }
+  });
+  
