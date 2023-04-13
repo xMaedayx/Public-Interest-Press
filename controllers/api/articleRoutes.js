30a5e6b6ece50e5baac6a2c158cfea110e511afb
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-04-13 11:58:25 -0700

    added post functionality to article and tips

--- controllers/api/articleRoutes.js
+++ controllers/api/articleRoutes.js
@@ -3,18 +3,18 @@ const { Article } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const newArticle = await Article.create({
-        title: req.body.title,
-        content: req.body.content,
-        author_id: req.body.author_id,
-      });
-  
-      res.status(200).json(newArticle);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  try {
+    const newArticle = await Article.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newArticle);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
   router.delete('/:id', withAuth, async (req, res) => {
     try {
--- public/js/newArticle.js
+++ public/js/newArticle.js
@@ -6,12 +6,12 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
+    const title = document.querySelector('#article-title').value.trim();
+    const content = document.querySelector('#article-content').value.trim();
     const user_id = document.querySelector('#user_id').value.trim();
   
     if (title && content) {
-      const response = await fetch(`/api/tips`, {
+      const response = await fetch(`/api/article`, {
         method: 'POST',
         body: JSON.stringify({ title, content, user_id }),
         headers: {
@@ -22,15 +22,15 @@ const newFormHandler = async (event) => {
       console.log(response);
 
       if (response.ok) {
-        document.location.replace('/tips');
+        document.location.replace('/');
       } else {
-        alert('Failed to create tip');
+        alert('Failed to create article');
       }
     }
   };
 
   document
-  .querySelector('#new-tip-form')
+  .querySelector('#new-article-form')
   .addEventListener('submit', newFormHandler);
 
 });
